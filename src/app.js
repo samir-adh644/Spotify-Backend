268408ce79709059a6fb6f45bcefa26e074f1e6e
Samir Adhikari <081bct071@ioepc.edu.np>
2026-05-13 16:52:07 +0545

    with cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ const cookieParser=require('cookie-parser');
 const authRoutes = require('./routes/auth.routes')
 const musicRoutes = require('./routes/music.routes')
 const albumRoutes = require('./routes/album.routes')
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173', // Your React URL (Vite default)
+    credentials: true                // Required to allow cookies
+}));
 
 
 app.use('/api/auth',authRoutes)
